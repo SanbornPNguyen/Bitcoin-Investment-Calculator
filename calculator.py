7783e6d10fbb0b60270625b6b74d199dfbe79e3d
Sanborn Nguyen <sanbornpnguyen@gmail.com>
2020-07-14 18:24:51 -0700

    Removed print statements

--- calculator.py
+++ calculator.py
@@ -12,12 +12,8 @@ def calculate_return(buy_amount_dollars, buy_date="Apr 28, 2013", sell_date="Jul
 	buy_low = float(df.loc[df['Date'] == buy_date]["Low"])
 	buy_mean = (buy_high + buy_low) / 2
 
-	print(buy_mean)
-
 	sell_high = float(df.loc[df['Date'] == sell_date]["High"])
 	sell_low = float(df.loc[df['Date'] == sell_date]["Low"])
 	sell_mean = (sell_high + sell_low) / 2
-
-	print(sell_mean)
 
 	return round(sell_mean * (buy_amount_dollars / buy_mean), 2)
